8ad43b16a6c2fa59ad082a8795bcf546066af2f4
Jabievaitakin <aitakin.jabi@gmail.com>
2023-12-10 16:47:39 +0400

    task is done

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+let numberOfChildren =1;
+let partnerName ="A";
+let georaphicLocation ="center of Baku"
+let jobTitle = "Developer"
+
+console.log(`You will be a ${jobTitle} in ${georaphicLocation}, and married to ${partnerName} with ${numberOfChildren} kids.`)
